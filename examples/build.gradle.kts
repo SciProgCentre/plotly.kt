eee7c7fedbf85159f9e281a0de7a88d9fb28fdda
ebsamorodova <katsam@passap.ru>
2020-07-24 19:14:17 +0300

    Test loading resources

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
 dependencies {
     implementation(project(":plotlykt-server"))
     implementation(kotlin("script-runtime"))
+    implementation("de.mpicbg.scicomp:krangl:0.13")
+    implementation("org.apache.commons:commons-csv:1.8")
 }
