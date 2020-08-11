157801a22717b27917bdd34041f9a600065d9a70
Alexander Nozik <altavir@gmail.com>
2020-08-11 05:49:51 +0000

    Space CI

---
+++ .space.kts
@@ -0,0 +1,10 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, refer to https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Build and run tests") {
+   gradlew("openjdk:11", "build")
+}
+
