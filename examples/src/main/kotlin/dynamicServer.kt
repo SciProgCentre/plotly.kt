67e638dc2bccf5e947a5e1135e9c03ac4332fe88
Alexander Nozik <altavir@gmail.com>
2020-11-20 10:29:06 +0300

    Move traces definition outside of `serve` scope in dynamicServer.kt

--- examples/src/main/kotlin/dynamicServer.kt
+++ examples/src/main/kotlin/dynamicServer.kt
@@ -20,18 +20,17 @@ import kotlin.math.sin
 
 fun main() {
 
+    val freq = 1.0 / 1000
+    val oscillationFreq = 1.0 / 10000
+
+    val x = (0..100).map { it.toDouble() / 100.0 }
+    val sinY = x.map { sin(2.0 * PI * it) }
+    val cosY = x.map { cos(2.0 * PI * it) }
+
+    val sinTrace = Trace(x, sinY) { name = "sin" }
+    val cosTrace = Trace(x, cosY) { name = "cos" }
+
     val server = Plotly.serve(port = 3872) {
-
-        val freq = 1.0 / 1000
-        val oscillationFreq = 1.0 / 10000
-
-        val x = (0..100).map { it.toDouble() / 100.0 }
-        val sinY = x.map { sin(2.0 * PI * it) }
-        val cosY = x.map { cos(2.0 * PI * it) }
-
-        val sinTrace = Trace(x, sinY) { name = "sin" }
-        val cosTrace = Trace(x, cosY) { name = "cos" }
-
 
         //root level plots go to default page
         page { plotly ->
@@ -60,21 +59,23 @@ fun main() {
             }
         }
 
-        GlobalScope.launch {
-            var time: Long = 0
-
-            while (isActive) {
-                delay(10)
-                time += 10
-                sinTrace.y.numbers = x.map { sin(2.0 * PI * (it + time.toDouble() * freq)) }
-                val cosAmp = cos(2.0 * PI * oscillationFreq * time)
-                cosTrace.y.numbers = x.map { cos(2.0 * PI * (it + time.toDouble() * freq)) * cosAmp }
-            }
-        }
         pushUpdates(50)       // start sending updates via websocket to the front-end
     }
 
     server.show()
+
+    //Start pushing updates
+    GlobalScope.launch {
+        var time: Long = 0
+
+        while (isActive) {
+            delay(10)
+            time += 10
+            sinTrace.y.numbers = x.map { sin(2.0 * PI * (it + time.toDouble() * freq)) }
+            val cosAmp = cos(2.0 * PI * oscillationFreq * time)
+            cosTrace.y.numbers = x.map { cos(2.0 * PI * (it + time.toDouble() * freq)) * cosAmp }
+        }
+    }
 
     println("Press Enter to close server")
     readLine()
