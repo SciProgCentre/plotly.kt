bf4aa20c37d092bb81b0f8d92dfb055cf059121e
Artificial <matej.berger@hotmail.com>
2021-01-27 14:23:07 +0100

    Add documentation to Table trace model and related schemes

--- plotlykt-core/src/commonMain/kotlin/kscience/plotly/models/Trace.kt
+++ plotlykt-core/src/commonMain/kotlin/kscience/plotly/models/Trace.kt
@@ -628,16 +628,40 @@ public class Domain : Scheme() {
 
 public class Hoverlabel : Scheme() {
 
+    /**
+     * Sets the background color of the hover labels for this trace.
+     * */
     public var bgcolor: Color = Color(this, "bgcolor".asName())
 
+    /**
+     * Sets the border color of the hover labels for this trace.
+     * */
     public var bordercolor: Color = Color(this, "bordercolor".asName())
 
+    /**
+     * Sets the font used in hover labels.
+     * */
     public var font: Font? by spec(Font)
 
+    /**
+     * Sets the horizontal alignment of the text content within hover label box. Has an effect
+     * only if the hover label text spans more two or more lines.
+     *
+     * Defaults to `'auto'`.
+     * */
     public var align: TraceValues = TraceValues(this, "align".asName())
 
+    /**
+     * Sets the default length (in number of characters) of the trace name in the hover labels for all traces.
+     * -1 shows the whole name regardless of length. 0-3 shows the first 0-3 characters, and an integer >3 will
+     * show the whole name if it is less than that many characters, but if it is longer, will truncate to
+     * `namelength - 3` characters and add an ellipsis.
+     * */
     public var namelength: Number by numberGreaterThan(-1)
 
+    /**
+     * Complementary property to [namelength] to allow passing a list of lengths.
+     * */
     public var namelengths: List<Number> by numberList(-1, key = "namelength".asName())
 
     public fun bgcolors(array: Iterable<Any>) {
