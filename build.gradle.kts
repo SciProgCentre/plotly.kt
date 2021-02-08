cfff9ef82e6c7b77f1d9e28b2f2ef4c6185ca5c8
Alexander Nozik <altavir@gmail.com>
2021-02-08 12:32:59 +0300

    0.3.1. Promote to release version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val githubProject by extra("plotly.kt")
 
 allprojects {
     group = "kscience.plotlykt"
-    version = "0.3.1-dev-5"
+    version = "0.3.1"
 
     repositories {
         mavenLocal()
