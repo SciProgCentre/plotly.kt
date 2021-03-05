1909b64411d8c09c1e49554cf6889c687cb53788
Alexander Nozik <altavir@gmail.com>
2021-03-05 22:26:45 +0300

    Fix documentation and examples build

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -5,9 +5,6 @@ plugins {
 repositories {
     mavenLocal()
     jcenter()
-    maven("https://dl.bintray.com/mipt-npm/dataforge")
-    maven("https://dl.bintray.com/mipt-npm/kscience")
-    maven("https://dl.bintray.com/mipt-npm/dev")
 }
 
 dependencies {
@@ -18,15 +15,8 @@ dependencies {
     implementation("org.apache.commons:commons-csv:1.8")
 }
 
-val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "11"
-}
-
-
-val compileTestKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "11"
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
 }
 
 // A workaround for https://youtrack.jetbrains.com/issue/KT-44101
